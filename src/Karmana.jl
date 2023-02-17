a3e33766b711dcd8145903ad0cc34bf9e9055d98
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-02-17 14:01:08 +0530

    Use all relevant packages

--- src/Karmana.jl
+++ src/Karmana.jl
@@ -1,13 +1,30 @@
 module Karmana
 
-using DataStructures, DataDeps
-
 # visualization packages
 using Makie 
-using GeoInterface, GeoInterfaceMakie, GeoMakie, Proj
-using QRCode, Polylabel
+using Makie.FileIO, Makie.GeometryBasics, Makie.PlotUtils, Makie.Colors
+using GeoInterfaceMakie, GeoMakie
+using TernaryDiagrams, PerceptualColourMaps
+# geographic information systems packages
+using GeoInterface, Proj
+# raster loading
+using Rasters
+# feature/vector geometry
+using Shapefile, ArchGDAL
+using Polylabel
+# utility packages
+using DataDeps
+using QRCode, Ghostscript_jll
+using DataStructures, NaNMath
+using DBInterface, HTTP
+# standard libraries
+using LinearAlgebra, Dates, Downloads, DelimitedFiles
 
-using Rasters, Shapefile
+assetpath(args::AbstractString...) = abspath(dirname(@__DIR__), "assets", args...)
 
+include("themes.jl")
+include("geom_utils.jl")
+
+include("ternary.jl")
 
 end
