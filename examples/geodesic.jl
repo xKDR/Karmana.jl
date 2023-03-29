77a1031d9417877fc3855b82eeeb11e6ef9601b2
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-03-29 15:52:32 +0530

    Basic geodesic example framework

---
+++ examples/geodesic.jl
@@ -0,0 +1,58 @@
+# Exploring the geodesic utilities that Karmana provides
+
+using Proj, GeoInterface, Rasters, CairoMakie, Karmana
+
+
+# Geodetic circles - the locus of all points which are physically `radius` away from the center
+initial_line = Karmana.get_geodetic_circle(72, 50, 9000)
+lines(initial_line; axis = (; aspect = DataAspect()))
+
+#
+
+fig, ax, circ_plt = lines(Karmana.get_geodetic_circle(72, 50, 20000); linewidth = 2, axis = (; aspect = DataAspect()))
+cent_plot = scatter!(ax, [Point2f(72, 50)])
+fig
+
+# Geodetically expanding lines - equal physical width lines
+poly_line = Karmana.line_to_geodetic_width_poly(initial_line, 2000)
+poly_line = Karmana.line_to_geodetic_width_poly(Point2f.(LinRange(0, 6, 100), sin.(LinRange(0, 6, 100))), 2000)
+
+poly(poly_line; axis = (; aspect = DataAspect()))
+
+
+
+
+# Rasters.rasterize works on this polygon!
+annular_polygon = Makie.GeometryBasics.Polygon(
+    Karmana.get_geodetic_circle(72, 50, 90000), 
+    [reverse(Karmana.get_geodetic_circle(72, 50, 20000))] # note the `reverse` here - this is for the intersection fill rule.
+    )
+
+poly(annular_polygon; axis = (; aspect = DataAspect()))
+
+# Those were the building blocks, but let's do something fun.  
+
+# Let's get the nightlights data for India, and calculate their nightlights over time.
+
+using CSV, DataFrames
+
+nightlights_raster = view(Raster("/Users/anshul/Documents/Business/India/XKDR/code/maps/DATA/updated_india.nc"; lazy = true), :, :, 1, :)
+
+xs, ys = dims(nightlights_raster)
+nightlights_bbox = Makie.BBox(extrema(xs)..., extrema(ys)...)
+
+city_dataset = CSV.read("/Users/anshul/Downloads/simplemaps_worldcities_basicv1.75/worldcities.csv", DataFrame)
+
+lats, lons = city_dataset[!, :lat], city_dataset[!, :lng]
+
+india_cities = dropmissing(city_dataset[in.(Point2f.(lons, lats), (nightlights_bbox,)), :])
+sort!(india_cities, :population; order = Base.Reverse)
+
+# Top 8 cities in India by population
+india_cities.city_positions = Point2{Float64}.(india_cities[!, :lng], india_cities[!, :lat])
+using Statistics
+lights_from_ring = Karmana.annular_ring.((Statistics.mean,), (view(nightlights_raster, :, :, i) for i in 1:size(nightlights_raster, 3)), india_cities.city_positions[1]..., 15000, 3000)
+f, a, p = lines(lights_from_ring, label = "Mean radiance", axis = (; title = "Mean nighttime light radiance around Mumbai over time", xlabel = "Time (months)", ylabel = "Radiance"))
+axislegend(a, position = :lt)
+a.title = "Mean nighttime light radiance around Delhi over time"
+f
