e0398ade5c656ddf1adf62b47c3e976e2870ba50
ayushpatnaikgit <ayushpatnaik@gmail.com>
2023-06-09 11:26:49 +0000

    Add function for cropping by geometry. Change Date to DateTime.

--- src/readnl.jl
+++ src/readnl.jl
@@ -3,7 +3,7 @@ function sort_files_by_date(folder_path, start_date=Date(0), end_date=Date(today
     function extract_date(filename)
         # Extract the date range using regex
         m = match(r"_(\d{8})-\d{8}_", filename)
-        return m == nothing ? nothing : Date(m[1], "yyyymmdd")
+        return m == nothing ? nothing : DateTime(Date(m[1], "yyyymmdd"))
     end
     # Get the list of file names in the directory
     files = readdir(folder_path)
@@ -50,5 +50,41 @@ function readnl(xlims = X(Rasters.Between(65.39, 99.94)), ylims = Y(Rasters.Betw
     cf_series = RasterSeries(cf_raster_list, Ti(sorted_dates))
     rad_datacube = Rasters.combine(rad_series, Ti)
     cf_datacube = Rasters.combine(cf_series, Ti)
+    return rad_datacube, cf_datacube
 end
 
+"""
+    readnl(geom, start_date = Date(2012, 04), end_date = Date(2023, 01))
+
+Read nighttime lights data from a specific directory and return two raster data cubes representing radiance and coverage. This function also crops the rasters based on the given geometry.
+
+# Arguments
+- `geom`: A geometry object, which will be used to crop the rasters. This could be an instance of `Geometry`, `Polygon`, `MultiPolygon`, etc. from a shapefile.
+- `start_date`: The start date (inclusive) of the period for which to load data. Should be an instance of `Date`. Default is `Date(2012, 04)`.
+- `end_date`: The end date (inclusive) of the period for which to load data. Should be an instance of `Date`. Default is `Date(2023, 01)`.
+
+# Returns
+Two `RasterDataCube` instances. The first contains the cropped radiance data, and the second contains the cropped coverage data. Each `RasterDataCube` includes data from the `start_date` to the `end_date`, sorted in ascending order.
+
+# Examples
+```julia
+using Geometries
+geom = Geometries.read("path_to_your_shapefile.shp")  # replace this with your actual shapefile
+start_date = Date(2015, 01)
+end_date = Date(2020, 12)
+rad_dc, cf_dc = readnl(geom, start_date, end_date)
+```
+"""
+function readnl(geom, start_date = Date(2012, 04), end_date = Date(2023, 01))
+    rad_path = "/mnt/giant-disk/nighttimelights/monthly/rad/"
+    rad_files, sorted_dates = sort_files_by_date(rad_path, start_date, end_date)
+    cf_path = "/mnt/giant-disk/nighttimelights/monthly/cf/"
+    cf_files, sorted_dates = sort_files_by_date(cf_path, start_date, end_date)
+    rad_raster_list = [crop(Raster(i, lazy = true), to = geom) for i in rad_path .* rad_files]
+    cf_raster_list = [crop(Raster(i, lazy = true), to = geom) for i in cf_path .* cf_files]
+    rad_series = RasterSeries(rad_raster_list, Ti(sorted_dates))
+    cf_series = RasterSeries(cf_raster_list, Ti(sorted_dates))
+    rad_datacube = Rasters.combine(rad_series, Ti)
+    cf_datacube = Rasters.combine(cf_series, Ti)
+    return rad_datacube, cf_datacube
+end
